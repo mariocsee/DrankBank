542ae15507ffc355f87211b029f7db24ead08d6d
mariocsee <mariocsee@gmail.com>
2016-11-21 20:25:25 +0100

    init commit

---
+++ app/src/main/java/drankbank/android/drankbank/MainActivity.java
@@ -0,0 +1,13 @@
+package drankbank.android.drankbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
